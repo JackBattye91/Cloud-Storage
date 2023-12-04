56a87eb81400ce12b20acca69981a86c6b0248b5
JackBattye91 <66002339+JackBattye91@users.noreply.github.com>
2023-12-04 21:05:13 +0000

    Updated IUser namespace

--- Cloud Storage/Interfaces/IUser.cs
+++ Cloud Storage/Interfaces/IUser.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Cloud_Storage.Interfaces {
+namespace CloudStorage.Interfaces {
     public interface IUser {
         string Id { get; }
         string Username { get; set; }
