92b8a67dd49d19cd98168ad28df174e53c0dc705
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2023-12-04 18:41:17 +0000

    Added IUser interface

---
+++ Cloud Storage/Interfaces/IUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cloud_Storage.Interfaces {
+    public interface IUser {
+        string Id { get; }
+        string Username { get; set; }
+        string Email { get; set; }
+        string Password { get; set; }
+        string PasswordSalt { get; set; }
+        bool Verified { get; set; }
+    }
+}
