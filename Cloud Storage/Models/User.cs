8a76653caafbd954820326430dd13bac86402f6f
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2023-12-05 20:15:31 +0000

    Added User model

---
+++ Cloud Storage/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using CloudStorage.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudStorage.Models
+{
+    public class User : IUser
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string PasswordSalt { get; set; } = string.Empty;
+        public bool Verified { get; set; } = false;
+    }
+}
