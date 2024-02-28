b5e98f13203ff2d5e4e6314061b126dea5255f0c
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-02-28 20:29:57 +0000

    Added Token model

---
+++ CloudStorage/Models/Token.cs
@@ -0,0 +1,14 @@
+﻿using CloudStorage.Interfaces;
+using Newtonsoft.Json;
+using System.ComponentModel;
+
+namespace CloudStorage.Models
+{
+    public class Token : IToken
+    {
+        public string TokenId { get; set; } = string.Empty;
+        public string WebToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime Expires { get; set; }
+    }
+}
