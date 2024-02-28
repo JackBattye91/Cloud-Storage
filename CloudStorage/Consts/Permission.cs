77803c48fed83e22bca16cd928772367da8597c8
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-02-28 09:22:29 +0000

    Added Permissions consts

---
+++ CloudStorage/Consts/Permission.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace CloudStorage.Consts
+{
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum Permission
+    {
+        None = 0,
+        Read,
+        Owner,
+        Contributer,
+        Admin
+    }
+}
