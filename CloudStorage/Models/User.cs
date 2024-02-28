212c0ab84af557472b75d5155b93436070ee6f5a
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-02-28 09:22:57 +0000

    Added User model

---
+++ CloudStorage/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using CloudStorage.Consts;
+using CloudStorage.Interfaces;
+using Newtonsoft.Json;
+
+namespace CloudStorage.Models
+{
+    public class User : IUser
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; } = string.Empty;
+
+        [JsonProperty(PropertyName = "username")]
+        public string Username { get; set; } = string.Empty;
+
+        [JsonProperty(PropertyName = "password")]
+        public string Password { get; set; } = string.Empty;
+
+        [JsonProperty(PropertyName = "passwordSalt")]
+        public string PasswordSalt { get; set; } = string.Empty;
+
+        [JsonProperty(PropertyName = "permissions")]
+        public Permission Permissions { get; set; } = Permission.None;
+    }
+}
