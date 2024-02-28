f466bf946c39b26ea2d1f8f2d57df0ba6884c018
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-02-28 09:22:45 +0000

    Added User interface

---
+++ CloudStorage/Interfaces/IUser.cs
@@ -0,0 +1,23 @@
+﻿using CloudStorage.Consts;
+using Newtonsoft.Json;
+
+namespace CloudStorage.Interfaces
+{
+    public interface IUser
+    {
+        [JsonProperty(PropertyName = "id")]
+        string Id { get; set; }
+
+        [JsonProperty(PropertyName = "username")]
+        string Username { get; set; }
+
+        [JsonProperty(PropertyName = "password")]
+        string Password { get; set; }
+
+        [JsonProperty(PropertyName = "passwordSalt")]
+        string PasswordSalt { get; set; }
+
+        [JsonProperty(PropertyName = "permissions")]
+        Permission Permissions { get; set; }
+    }
+}
