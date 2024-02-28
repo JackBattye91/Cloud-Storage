12e420b697d6e7ef036b078dd11497df12b72914
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-02-28 09:22:40 +0000

    Added BlobDetail interface

---
+++ CloudStorage/Interfaces/IBlobDetail.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudStorage.Interfaces
+{
+    public interface IBlobDetail
+    {
+        [JsonProperty(PropertyName = "id")]
+        string Id { get; set; }
+
+        [JsonProperty(PropertyName = "fileName")]
+        string FileName { get; set; }
+
+        [JsonProperty(PropertyName = "containerName")]
+        string ContainerName { get; set; }
+
+        [JsonProperty(PropertyName = "userId")]
+        string UserId { get; set; }
+
+        [JsonProperty(PropertyName = "blobname")]
+        string BlobName { get; set; }
+
+        [JsonProperty(PropertyName = "fileExtension")]
+        string FileExtension { get; set; }
+
+        [JsonProperty(PropertyName = "deleted")]
+        bool Deleted { get; set; }
+
+        [JsonProperty(PropertyName = "thumbnail")]
+        string ThumbNail { get; set; }
+    }
+}
