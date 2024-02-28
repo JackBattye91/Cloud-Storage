8220037d95a44d06e2626f2b4a00036d9ea604f7
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-02-28 09:22:52 +0000

    Added BlobDetail model

---
+++ CloudStorage/Models/BlobDetail.cs
@@ -0,0 +1,32 @@
+﻿using CloudStorage.Interfaces;
+using Newtonsoft.Json;
+
+namespace CloudStorage.Models
+{
+    public class BlobDetail : IBlobDetail
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; } = string.Empty;
+
+        [JsonProperty(PropertyName = "fileName")]
+        public string FileName { get; set; } = string.Empty;
+
+        [JsonProperty(PropertyName = "containerName")]
+        public string ContainerName { get; set; } = string.Empty;
+
+        [JsonProperty(PropertyName = "userId")]
+        public string UserId { get; set; } = string.Empty;
+
+        [JsonProperty(PropertyName = "blobname")]
+        public string BlobName { get; set; } = string.Empty;
+
+        [JsonProperty(PropertyName = "fileExtension")]
+        public string FileExtension { get; set; } = string.Empty;
+
+        [JsonProperty(PropertyName = "deleted")]
+        public bool Deleted { get; set; } = false;
+
+        [JsonProperty(PropertyName = "thumbnail")]
+        public string ThumbNail { get; set; } = string.Empty;
+    }
+}
