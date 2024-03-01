c255cc166a0f952b74163976417c742a696b716b
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-03-01 09:39:58 +0000

    Add AppState

---
+++ CloudStorage.SPA/Models/AppState.cs
@@ -0,0 +1,11 @@
+﻿using CloudStorage.Interfaces;
+using CloudStorage.Models;
+
+namespace CloudStorage.SPA.Models
+{
+    public class AppState
+    {
+        public IUser? CurrentUser { get; set; } = null;
+        public IList<BlobDetail>? BlobDetails { get; set; } = null;
+    }
+}
