60e6321221f00f8fde6524deda1893b364e13485
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-03-01 11:55:43 +0000

    Updated AppState

--- CloudStorage.SPA/Models/AppState.cs
+++ CloudStorage.SPA/Models/AppState.cs
@@ -5,6 +5,7 @@ namespace CloudStorage.SPA.Models
 {
     public class AppState
     {
+        public static AppState Instance { get; set; } = new AppState();
         public IUser? CurrentUser { get; set; } = null;
         public IList<BlobDetail>? BlobDetails { get; set; } = null;
     }
