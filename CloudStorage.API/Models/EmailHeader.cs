cf35da7028ef675b0548dd31cfbac69bd2f7de3b
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-03-25 09:32:31 +0000

    Added Email related models

---
+++ CloudStorage.API/Models/EmailHeader.cs
@@ -0,0 +1,13 @@
+﻿using JB.Email.Interfaces;
+
+namespace CloudStorage.API.Models
+{
+    public class EmailHeader : IEmailHeader
+    {
+        public string Sender { get; set; } = string.Empty;
+        public IEnumerable<string> To { get; set; } = new List<string>();
+        public IEnumerable<string> Cc { get; set; } = new List<string>();
+        public IEnumerable<string> Bcc { get; set; } = new List<string>();
+        public string Subject { get; set; } = string.Empty;
+    }
+}
