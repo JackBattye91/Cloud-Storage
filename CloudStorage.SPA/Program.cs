0a87ba202b05fa9fb0c454aed7e5fc26090e540f
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-03-25 15:24:26 +0000

    Updated Program

--- CloudStorage.SPA/Program.cs
+++ CloudStorage.SPA/Program.cs
@@ -4,6 +4,10 @@ using CloudStorage.SPA.Models;
 using Microsoft.AspNetCore.Identity;
 using MudBlazor.Services;
 using Blazored.SessionStorage;
+using Blazored.LocalStorage;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using CloudStorage.SPA;
 
 var builder = WebApplication.CreateBuilder(args);
 AppSettings? appSettings = builder.Configuration.Get<AppSettings>();
@@ -17,10 +21,26 @@ builder.Services.AddHttpClient("api", x => {
     x.BaseAddress = new Uri(appSettings.ApiBaseUrl);
 }).AddHttpMessageHandler<JwtDelegationHandler>();
 
-
 builder.Services.AddMudServices();
 builder.Services.AddTransient<JwtDelegationHandler>();
-builder.Services.AddBlazoredSessionStorage();
+builder.Services.AddBlazoredSessionStorage(config => {
+    config.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+    config.JsonSerializerOptions.IgnoreReadOnlyProperties = true;
+    config.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    config.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    config.JsonSerializerOptions.ReadCommentHandling = JsonCommentHandling.Skip;
+    config.JsonSerializerOptions.WriteIndented = false;
+});
+builder.Services.AddBlazoredLocalStorage(config =>
+{
+    config.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+    config.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    config.JsonSerializerOptions.IgnoreReadOnlyProperties = true;
+    config.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    config.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    config.JsonSerializerOptions.ReadCommentHandling = JsonCommentHandling.Skip;
+    config.JsonSerializerOptions.WriteIndented = false;
+});
 
 var app = builder.Build();
 
