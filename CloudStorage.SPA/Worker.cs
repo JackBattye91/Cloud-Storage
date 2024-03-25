961dcfcd0e035dca66049e2b9a49ca68e5dc6f4c
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-03-25 15:24:15 +0000

    Added Worker class

---
+++ CloudStorage.SPA/Worker.cs
@@ -0,0 +1,27 @@
+﻿namespace CloudStorage.SPA
+{
+    public class Worker
+    {
+        public static IDictionary<string, string> GetQueryParameters(string pRelativeUri)
+        {
+            IDictionary<string, string> parameters = new Dictionary<string, string>();
+            int queryStart = pRelativeUri.IndexOf('?') + 1;
+            if (queryStart != -1)
+            {
+                string pQueries = pRelativeUri.Substring(queryStart);
+                string[] nameAndValues = pQueries.Split('&');
+
+                foreach(string n  in nameAndValues)
+                {
+                    string[] nameValue = n.Split("=");
+                    if (nameValue.Length == 2)
+                    {
+                        parameters.Add(nameValue[0], nameValue[1]);
+                    }
+                }
+            }
+
+            return parameters;
+        }
+    }
+}
