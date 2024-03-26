3ec73bf2530e01d2509283f38bbee2f860b0f45d
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-03-26 10:30:10 +0000

    Added Breadcrumb model

---
+++ CloudStorage.SPA/Models/Breadcrumb.cs
@@ -0,0 +1,9 @@
+﻿namespace CloudStorage.SPA.Models
+{
+    public class Breadcrumb
+    {
+        public string DisplayName { get; set; } = string.Empty;
+        public string? Link { get; set; }
+        public string? Icon { get; set; }
+    }
+}
