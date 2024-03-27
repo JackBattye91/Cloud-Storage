66ecd0b7702352ef6942709d2a971f213fb97110
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-03-27 19:54:39 +0000

    Updated Worker added CreateThumbnail from Stream

--- CloudStorage.API/Worker.cs
+++ CloudStorage.API/Worker.cs
@@ -103,6 +103,25 @@ namespace CloudStorage.API
             return mStream.ToArray();
         }
 
+        public static async Task<byte[]> CreateThumbnail(Stream stream, int pMaxWidth, int pMaxHeight)
+        {
+            Image baseImage = await Image.LoadAsync(stream);
+
+            double ratioX = (double)pMaxWidth / baseImage.Width;
+            double ratioY = (double)pMaxHeight / baseImage.Height;
+            double ratio = Math.Min(ratioX, ratioY);
+
+            int newWidth = (int)(baseImage.Width * ratio);
+            int newHeight = (int)(baseImage.Height * ratio);
+
+            baseImage.Mutate(x => x.Resize(newWidth, newHeight));
+
+            using MemoryStream mStream = new MemoryStream();
+            baseImage.Save(mStream, new JpegEncoder());
+
+            return mStream.ToArray();
+        }
+
         public static string CreatePermissionsList(IList<Permission> pPermissions)
         {
             StringBuilder sb = new StringBuilder();
@@ -117,6 +136,21 @@ namespace CloudStorage.API
             }
 
             return sb.ToString();
+        }
+
+        public static async Task<Stream> CopyTo(Stream pBaseStream)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            byte[] bytes = new byte[512];
+            int bytesRead = 0;
+
+            do
+            {
+                bytesRead = await pBaseStream.ReadAsync(bytes, 0, 512);
+                memoryStream.Write(bytes, 0, bytesRead);
+            } while (bytesRead > 0);
+
+            return memoryStream;
         }
     }
 }
