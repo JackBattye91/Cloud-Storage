075ad00c6e25467b85b9ffef1ac1b5aa5efd27d7
JackBattye91 <66002339+JackBattye91@users.noreply.github.com>
2024-03-27 11:28:46 +0000

    Updated AppState with Token property and Load function

--- CloudStorage.SPA/Models/AppState.cs
+++ CloudStorage.SPA/Models/AppState.cs
@@ -1,12 +1,35 @@
 ﻿using CloudStorage.Interfaces;
 using CloudStorage.Models;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 
 namespace CloudStorage.SPA.Models
 {
     public class AppState
     {
-        public static AppState Instance { get; set; } = new AppState();
-        public IUser? CurrentUser { get; set; } = null;
-        public IList<BlobDetail>? BlobDetails { get; set; } = null;
+        public static AppState? Instance { get; set; }
+        public IUser? CurrentUser { get; set; }
+        //public IList<IBlobDetail>? BlobDetails { get; set; } = null;
+        public IToken? Token { get; set; }
+
+        public static async Task Load(ProtectedLocalStorage pProtectedStorage)
+        {
+            if (Instance == null)
+            {
+                Instance = new AppState();
+            }
+
+            try
+            {
+                ProtectedBrowserStorageResult<User>? userValue = await pProtectedStorage.GetAsync<User>("user");
+                Instance!.CurrentUser = userValue?.Value;
+
+                ProtectedBrowserStorageResult<Token>? tokenValue = await pProtectedStorage.GetAsync<Token>("token");
+                Instance!.Token = tokenValue?.Value;
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
     }
 }
