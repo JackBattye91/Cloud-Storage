37824db6c784fc45e97a79b7ee5eac5e9b17b7ba
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-03-28 09:19:50 +0000

    Added heartbeat controller

---
+++ CloudStorage.API/Controllers/HeartbeatController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CloudStorage.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HeartbeatController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Response.ContentType = "text/plain";
+            return new OkObjectResult("Heartbeat");
+        }
+    }
+}
