204489ed522279c72ac5ffc92234d1c1ffddad6d
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-03-28 09:13:32 +0000

    Updated FileUpload to remove unused properties and adding CreateThumbanil

--- CloudStorage/Models/FileUpload.cs
+++ CloudStorage/Models/FileUpload.cs
@@ -4,10 +4,8 @@
     {
         public string FileName { get; set; } = string.Empty;
         public string ContainerName { get; set; } = string.Empty;
-        public string DataBase64 { get; set; } = string.Empty;
-        public byte[]? Data { get; set; }
-        public Stream? DataStream { get; set; }
         public string FileExtension { get; set; } = string.Empty;
         public bool IsPrivate { get; set; } = false;
+        public bool CreateThumbnail { get; set; } = true;
     }
 }
