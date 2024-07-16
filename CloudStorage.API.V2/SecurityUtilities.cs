a0972b1ffc44b424fb33a5aab9802f6595b6293f
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-07-16 18:18:05 +0100

    Added SecurityUtilities

---
+++ CloudStorage.API.V2/SecurityUtilities.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace CloudStorage.API.V2
+{
+    public class SecurityUtilities
+    {
+        public static bool ValidatePassword(string pPassword)
+        {
+            return Regex.Match(pPassword, Consts.RegEx.PASSWORD_VALIDATION).Success;
+        }
+    }
+}
