02c5699233d6695d3c3f56b50f550276d9ca743d
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-07-16 18:17:26 +0100

    Added AccountController

---
+++ CloudStorage.API.V2/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CloudStorage.API.V2.Models.DTOs;
+
+namespace CloudStorage.API.V2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly ILogger<AccountController> _logger;
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult CreateAccount(UserDTO user)
+        {
+            try
+            {
+                if (!SecurityUtilities.ValidatePassword(user.Password))
+                {
+                    return BadRequest();
+                }
+
+
+
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Create Account Failed");
+                return StatusCode(500);
+            }
+        }
+    }
+}
