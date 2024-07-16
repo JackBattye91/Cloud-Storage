b2c1c854421714cf758d0b61044fe6ea7dc8770a
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-07-16 18:17:17 +0100

    Added RegEx consts

---
+++ CloudStorage.API.V2/Consts/RegEx.cs
@@ -0,0 +1,7 @@
+﻿namespace CloudStorage.API.V2.Consts
+{
+    public class RegEx
+    {
+        public const string PASSWORD_VALIDATION = "^(?=.+[0-9])(?=.+[a-z])(?=.+[A-Z])(?=.+[@#$%^&+=.\\-_*])(.{8,})$";
+    }
+}
