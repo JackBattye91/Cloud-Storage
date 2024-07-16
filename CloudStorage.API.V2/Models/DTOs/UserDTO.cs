b57ca5fe2f7f45b872136a07bd9da0c3073dfc4d
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-07-16 18:17:41 +0100

    Updated UserDTO

--- CloudStorage.API.V2/Models/DTOs/UserDTO.cs
+++ CloudStorage.API.V2/Models/DTOs/UserDTO.cs
@@ -4,9 +4,9 @@ namespace CloudStorage.API.V2.Models.DTOs
 {
     public class UserDTO
     {
-        public string Id { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
         public string Forenames { get; set; } = string.Empty;
         public string Surname { get; set; } = string.Empty;
     }
