b3b24df295b13e5578de3f98e71b4e44158c9140
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-08-07 11:55:52 +0100

    Updated Database consts with RoleContainer

--- CloudStorage.API.V2/Consts/Database.cs
+++ CloudStorage.API.V2/Consts/Database.cs
@@ -2,7 +2,8 @@
 {
     public class Database
     {
-        public const string UserContainer = "";
+        public const string UserContainer = "User";
+        public const string RoleContainer = "Role";
         public const string RefreshTokenContainer = "RefreshToken";
 
     }
