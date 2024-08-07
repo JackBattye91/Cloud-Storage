641b123a862b4c051fd93b6067950e88c4726cf9
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-08-07 11:42:57 +0100

    Updated BlobController with GetBlobDetails

--- CloudStorage.API.V2/Controllers/BlobController.cs
+++ CloudStorage.API.V2/Controllers/BlobController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using JB.Blob;
+using JB.Common;
 
 namespace CloudStorage.API.V2.Controllers
 {
@@ -6,11 +8,35 @@ namespace CloudStorage.API.V2.Controllers
     [Route("[controller]")]
     public class BlobController : ControllerBase
     {
+        private readonly ILogger<BlobController> logger;
+        private readonly IWrapper _blobService;
+
+        public BlobController(ILogger<BlobController> logger, IWrapper blobService)
+        {
+            this.logger = logger;
+            _blobService = blobService;
+        }
+
         [HttpGet]
         [Route("details")]
         public IActionResult GetBlobDetails()
         {
-            return Ok();
+            IReturnCode rc = new ReturnCode();
+
+            try
+            {
+                if (rc.Success)
+                {
+
+                }
+
+                throw new Exception();
+            }
+            catch (Exception ex)
+            {
+                rc.AddError(new Error(ex));
+                return StatusCode(500, rc);
+            }
         }
     }
 }
