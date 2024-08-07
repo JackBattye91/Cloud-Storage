14c4a001cb2a1aa2aae6fc0fd90f5f33e858d23b
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-08-07 11:43:48 +0100

    Added RefreshToken model

---
+++ CloudStorage.API.V2/Models/RefreshToken.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace CloudStorage.API.V2.Models
+{
+    public class RefreshToken
+    {
+        [JsonProperty(propertyName: "id")]
+        public string Id { get; set; } = string.Empty;
+        public string Subject { get; set; } = string.Empty;
+        public DateTime IssuedAt { get; set; }
+        public DateTime Expires { get; set; }
+    }
+}
