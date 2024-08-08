1c490f5896f4419e074b05a2f28aa8208378e523
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-08-08 17:12:21 +0100

    Updated BlobService

--- CloudStorage.API.V2/Services/BlobService.cs
+++ CloudStorage.API.V2/Services/BlobService.cs
@@ -1,13 +1,71 @@
 ﻿
 
+using System.IO;
+
 namespace CloudStorage.API.V2.Services
 {
     public interface IBlobService
     {
-
+        Task<Stream> GetBlobStreamAsync(string container, string name);
+        Task<byte[]> GetBlobDataAsync(string container, string name);
+        Task UploadBlobAsync(string container, string name, Stream stream);
+        Task UploadBlobAsync(string container, string name, byte[] data);
     }
 
     public class BlobService : IBlobService
     {
+        private readonly ILogger<BlobService> _logger;
+        private readonly JB.Blob.IWrapper _blobWrapper;
+
+        public BlobService(ILogger<BlobService> logger, JB.Blob.IWrapper blobWrapper)
+        {
+            _logger = logger;
+            _blobWrapper = blobWrapper;
+        }
+
+        public async Task<Stream> GetBlobStreamAsync(string container, string name)
+        {
+            var getStreamRc = await _blobWrapper.GetBlobAsStreamAsync(container, name);
+
+            if (getStreamRc.Success)
+            {
+                return getStreamRc.Data!;
+            }
+
+            throw new Exception("Unable to get blob as stream");
+        }
+        public async Task<byte[]> GetBlobDataAsync(string container, string name)
+        {
+            var getStreamRc = await _blobWrapper.GetBlobAsArrayAsync(container, name);
+
+            if (getStreamRc.Success)
+            {
+                return getStreamRc.Data!;
+            }
+
+            throw new Exception("Unable to get blob data");
+        }
+        public async Task UploadBlobAsync(string container, string name, Stream stream)
+        {
+            var getStreamRc = await _blobWrapper.UploadBlobAsync(stream, container, name);
+
+            if (getStreamRc.Success)
+            {
+                return;
+            }
+
+            throw new Exception("Unable to get blob data");
+        }
+        public async Task UploadBlobAsync(string container, string name, byte[] data) {
+            var getStreamRc = await _blobWrapper.UploadBlobAsync(data, container, name);
+
+            if (getStreamRc.Success)
+            {
+                return;
+            }
+
+            throw new Exception("Unable to get blob data");
+        }
+
     }
 }
