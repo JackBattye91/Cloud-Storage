86467194c68118fd6ce3a98cf0a2930d8b0a3912
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-08-08 17:11:15 +0100

    Updated Accountcontroller CreateAccount

--- CloudStorage.API.V2/Controllers/AccountController.cs
+++ CloudStorage.API.V2/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using CloudStorage.API.V2.Models.DTOs;
+using CloudStorage.API.V2.Models;
+using CloudStorage.API.V2.Services;
 
 namespace CloudStorage.API.V2.Controllers
 {
@@ -9,24 +11,36 @@ namespace CloudStorage.API.V2.Controllers
     public class AccountController : ControllerBase
     {
         private readonly ILogger<AccountController> _logger;
-        public AccountController(ILogger<AccountController> logger)
+        private readonly IUserService _userService;
+
+        public AccountController(ILogger<AccountController> logger, IUserService userService)
         {
             _logger = logger;
+            _userService = userService;
         }
 
         [HttpPost]
-        public IActionResult CreateAccount(UserDTO user)
+        public async Task<IActionResult> CreateAccount(UserDTO pUser)
         {
             try
             {
-                if (!SecurityUtilities.ValidatePassword(user.Password))
+                if (!SecurityUtilities.ValidatePassword(pUser.Password))
+                {
+                    return BadRequest();
+                }
+                User? user = Converter.Convert<UserDTO, User>(pUser);
+
+                if (user == null)
                 {
                     return BadRequest();
                 }
 
+                user.Created = DateTime.UtcNow;
+                user.Updated = DateTime.UtcNow;
+                user = await _userService.CreateAsync(user);
+                pUser = Converter.Convert(user) ?? new UserDTO();
 
-
-                return Ok(user);
+                return Ok(pUser);
             }
             catch (Exception ex)
             {
