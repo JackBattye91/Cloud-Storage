642add0dc16d5577dbd3a9de9f272e9c2ae37c2c
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-08-27 17:46:46 +0100

    Added User model

---
+++ CloudStorage.SPA.V2/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace CloudStorage.SPA.V2.Models
+{
+    public class User
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Forenames { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+    }
+}
