a8e07c6abd67eb209d5c82e50def11c1765cec4d
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-08-27 17:46:31 +0100

    Added AccessToken

---
+++ CloudStorage.SPA.V2/Models/AccessToken.cs
@@ -0,0 +1,10 @@
+﻿namespace CloudStorage.SPA.V2.Models
+{
+    public class AccessToken
+    {
+        public string Token { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime Expires { get; set; }
+        public User? User { get; set; }
+    }
+}
