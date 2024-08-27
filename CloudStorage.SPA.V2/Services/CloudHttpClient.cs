fdd4cfe1afd549986649364dc2dde8357c133ef7
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-08-27 17:47:03 +0100

    Added CloudHttpClient

---
+++ CloudStorage.SPA.V2/Services/CloudHttpClient.cs
@@ -0,0 +1,26 @@
+﻿namespace CloudStorage.SPA.V2.Services
+{
+    public class CloudHttpClient : HttpClient
+    {
+        private readonly CloudStorageAuthenticationState _authenticationState;
+
+        public CloudHttpClient(CloudStorageAuthenticationState authenticationState)
+        {
+            _authenticationState = authenticationState;
+        }
+
+        public override HttpResponseMessage Send(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            
+
+            return base.Send(request, cancellationToken);
+        }
+
+        public override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return base.SendAsync(request, cancellationToken);
+        }
+
+
+    }
+}
