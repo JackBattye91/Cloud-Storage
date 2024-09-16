5bb7a939f0dee8924dcfa685f7b9d4870a6b13bb
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-09-16 18:47:48 +0100

    Updated BlobService

--- CloudStorage.API.V2/Services/BlobService.cs
+++ CloudStorage.API.V2/Services/BlobService.cs
@@ -1,4 +1,9 @@
 ﻿using System.IO;
+using Azure.Storage;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using CloudStorage.API.V2.Models;
+using Microsoft.Extensions.Options;
 
 namespace CloudStorage.API.V2.Services
 {
@@ -15,61 +20,58 @@ namespace CloudStorage.API.V2.Services
     public class BlobService : IBlobService
     {
         private readonly ILogger<BlobService> _logger;
-        private readonly JB.Blob.IWrapper _blobWrapper;
+        private readonly AppSettings _appSettings;
+        private readonly BlobServiceClient _blobService;
 
-        public BlobService(ILogger<BlobService> logger, JB.Blob.IWrapper blobWrapper)
+        public BlobService(ILogger<BlobService> logger, IOptions<AppSettings> appSettings)
         {
             _logger = logger;
-            _blobWrapper = blobWrapper;
+            _appSettings = appSettings.Value;
+            _blobService = new BlobServiceClient(_appSettings.Blob.ConnectionString);
         }
 
         public async Task<Stream> GetBlobStreamAsync(string container, string name)
         {
-            var getStreamRc = await _blobWrapper.GetBlobAsStreamAsync(container, name);
+            var containerClient = _blobService.GetBlobContainerClient(container);
+            var blobClient = containerClient.GetBlobClient(name);
+            var stream = await blobClient.DownloadStreamingAsync();
 
-            if (getStreamRc.Success)
-            {
-                return getStreamRc.Data!;
-            }
-
-            throw new Exception("Unable to get blob as stream");
+            return stream.Value.Content;
         }
         public async Task<byte[]> GetBlobDataAsync(string container, string name)
         {
-            var getStreamRc = await _blobWrapper.GetBlobAsArrayAsync(container, name);
+            var containerClient = _blobService.GetBlobContainerClient(container);
+            var blobClient = containerClient.GetBlobClient(name);
+            var blobDownload = await blobClient.DownloadContentAsync();
 
-            if (getStreamRc.Success)
+            var getStreamRc = blobDownload.GetRawResponse().Content;
+
+            if (getStreamRc == null)
             {
-                return getStreamRc.Data!;
+                throw new Exception("GetBlobDataAsync Failed");
             }
 
-            throw new Exception("Unable to get blob data");
+            return getStreamRc.ToArray();
         }
         public async Task UploadBlobAsync(string container, string name, Stream stream)
         {
-            var getStreamRc = await _blobWrapper.UploadBlobAsync(stream, container, name);
-
-            if (getStreamRc.Success)
-            {
-                return;
-            }
-
-            throw new Exception("Unable to get blob data");
+            var containerClient = _blobService.GetBlobContainerClient(container);
+            var blobClient = containerClient.GetBlobClient(name);
+            await blobClient.UploadAsync(stream);
         }
         public async Task UploadBlobAsync(string container, string name, byte[] data) {
-            var getStreamRc = await _blobWrapper.UploadBlobAsync(data, container, name);
+            var containerClient = _blobService.GetBlobContainerClient(container);
+            var blobClient = containerClient.GetBlobClient(name);
 
-            if (getStreamRc.Success)
-            {
-                return;
-            }
-
-            throw new Exception("Unable to get blob data");
+            BinaryData binaryData = new BinaryData(data);
+            await blobClient.UploadAsync(binaryData);
         }
 
         public async Task DeleteBlobAsync(string container, string name)
         {
-            
+            var containerClient = _blobService.GetBlobContainerClient(container);
+            var blobClient = containerClient.GetBlobClient(name);
+            var deleteResponse = await blobClient.DeleteAsync();
         }
     }
 }
