4d69def7515db094459b67cfdca326c75229abb5
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2024-09-26 10:40:55 +0100

    Updated BlobDetails model with IsDirectory

--- CloudStorage.SPA.V2/Models/BlobDetail.cs
+++ CloudStorage.SPA.V2/Models/BlobDetail.cs
@@ -7,5 +7,6 @@
         public string Description { get; set; } = string.Empty;
         public string MimeType { get; set; } = string.Empty;
         public string Extension { get; set; } = string.Empty;
+        public bool IsDirectory { get; set; } = false;
     }
 }
